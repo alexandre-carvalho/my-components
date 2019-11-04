d6401dc7659d5bf7f144333e2b8156ee01354980
Alexandre Carvalho <alexandre.carvalho@maltacobrancas.com.br>
2019-11-04 17:50:04 -0300

    Testes de efeitos

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -5,13 +5,17 @@ import React from 'react';
 import './index.scss';
 import Space from '../../components/effects/space';
 import Particles from '../../components/effects/particles';
+import Waves from '../../components/effects/waves';
+import Connections from '../../components/effects/connections/';
 
 const Home = () => {
     return (
         <div className="banner">
             <div className="children">
                 {/* <Space /> */}
-                <Particles />
+                {/* <Particles /> */}
+                {/* <Waves /> ERRO */}
+                <Connections />
             </div>
         </div>
     )
