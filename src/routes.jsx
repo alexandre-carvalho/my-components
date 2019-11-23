4b130620ed8879d2826981ec64b07dd3b2940a93
Alexandre Carvalho <alexandre.carvalho@maltacobrancas.com.br>
2019-11-23 09:56:44 -0300

    Removido login

--- src/routes.jsx
+++ src/routes.jsx
@@ -6,14 +6,10 @@ import { history } from './redux/store';
 import { Switch, Route } from "react-router";
 import { ConnectedRouter } from 'connected-react-router';
 
-import Login from "./pages/login";
 import Home from "./pages/home";
 
 export default () => <ConnectedRouter history={history}>
     <Switch>
-        {/* Página Login
-        <Route path="/" component={Login} /> */}
-        {/* Página Inicial */}
         <Route exact path="/" component={Home} />
     </Switch>
 </ConnectedRouter>
